491d0d189e34bfb8d4d2c87d182ff2e66cee2252
neeraj969 <neerajthapa831@gmail.com>
2025-08-24 09:40:38 +0545

    Updated RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -38,6 +38,9 @@
                    >
                      <option value="">Select</option>
                      {classes.map(cls => (
+                     )
+                     )
+                     }
 @@ .. @@
                  <div className="relative">
 -                  <BookMarked className="absolute left-3 top-3.5 h-4 w-4 text-purple-400" />
@@ -52,6 +55,9 @@
                    >
                      <option value="">Select</option>
                      {faculties.map(faculty => (
+                     )
+                     )
+                     }
 @@ .. @@
               <div className="relative">
 -                <Mail className="absolute left-3 top-3.5 h-4 w-4 text-purple-400" />
